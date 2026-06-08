6e0d9db74d55c8cad0fd11fe629553fb1b90036f
shivgupta272007-dotcom <shivgupta272007@gmail.com>
2026-06-09 01:10:08 +0530

    Add day7 folder

---
+++ day7/Q27.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sumOfDigits(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return (n % 10) + sumOfDigits(n / 10);
+}
+
+int main() {
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        num = -num;
+    }
+
+    printf("Sum of digits: %d\n", sumOfDigits(num));
+
+    return 0;
+}
